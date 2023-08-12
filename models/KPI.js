70e73171d1669fcdb748911e624cfa1cb8bf8861
gamitej <gamitejpratapsingh998@gmail.com>
2023-08-12 17:25:08 +0530

    kpi schema done

--- models/KPI.js
+++ models/KPI.js
@@ -4,6 +4,25 @@ const { loadType } = require("mongoose-currency");
 const Schema = mongoose.Schema;
 loadType(mongoose);
 
+const DailySchema = Schema(
+  {
+    date: String,
+    revenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    expenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+  },
+  {
+    toJSON: { getters: true },
+  }
+);
+
 const MonthlySchema = Schema(
   {
     monthly: String,
@@ -33,32 +52,39 @@ const MonthlySchema = Schema(
   }
 );
 
-const KpiSchema = new Schema({
-  totalProfit: {
-    type: mongoose.Types.Currency,
-    currency: "USD",
-    get: (v) => v / 100,
-  },
-  totalRevenue: {
-    type: mongoose.Types.Currency,
-    currency: "USD",
-    get: (v) => v / 100,
-  },
-  totalExpenses: {
-    type: mongoose.Types.Currency,
-    currency: "USD",
-    get: (v) => v / 100,
-  },
-  expensesByCategory: {
-    type: Map,
-    of: {
+const KpiSchema = new Schema(
+  {
+    totalProfit: {
       type: mongoose.Types.Currency,
       currency: "USD",
       get: (v) => v / 100,
     },
+    totalRevenue: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    totalExpenses: {
+      type: mongoose.Types.Currency,
+      currency: "USD",
+      get: (v) => v / 100,
+    },
+    expensesByCategory: {
+      type: Map,
+      of: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v / 100,
+      },
+    },
+    monthlyData: [MonthlySchema],
+    dailyData: [DailySchema],
   },
-  monthlyData: [MonthlySchema],
-});
+  {
+    toJSON: { getters: true },
+    timestamps: true,
+  }
+);
 
 // export
 const KPI = mongoose.model("Kpi", KpiSchema);
--- routes/kpi.js
+++ routes/kpi.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
+const KPI = require("../models/KPI.js");
 
 router.get("/", async (req, res, next) => {
   try {
     const kpisData = await KPI.find();
+    return res.status(200).json(kpisData);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
