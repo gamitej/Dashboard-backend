debd404d1d4dda732fc27ec3756c9e18452e504e
gamitej <gamitejpratapsingh998@gmail.com>
2023-08-12 16:48:24 +0530

    mongoose setup done

--- index.js
+++ index.js
@@ -3,19 +3,37 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-// config
+/* CONFIGURATIONS */
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(helmet());
 
-// env
+/* ENV  */
 const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+/* MONGO-DB CONNECTION */
+async function connectToMongoDb() {
+  try {
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    /* SERVER */
+    app.listen(PORT, () => {
+      console.log(`Connected to MongoDB ✅ & listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error);
+  }
+}
+connectToMongoDb();
